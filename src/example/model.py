e4a439983dcb627503a8bdc75d27b212e4b718c3
Elliot Hill <elliot.douglas.hill@gmail.com>
2023-11-14 16:34:50 -0500

    add one to hidden dim

--- src/example/model.py
+++ src/example/model.py
@@ -5,7 +5,7 @@ from torch import Tensor
 class MyNetwork(Module):
     def __init__(self, input_dim: int, output_dim: int, hidden_dim: int) -> None:
         super().__init__()
-        self.linear = Linear(in_features=input_dim, out_features=hidden_dim)
+        self.linear = Linear(in_features=input_dim, out_features=hidden_dim + 1)
         self.hidden_layer = Linear(in_features=hidden_dim, out_features=output_dim)
         self.model = Sequential(self.linear, self.hidden_layer)
 
